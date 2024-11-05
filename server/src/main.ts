bb2069379f903d0a0008500082284a090b52f7e4
Nam Phan <pphnam21@clc.fitus.edu.vn>
2024-11-05 21:45:49 +0700

    chore: change origin url

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,10 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Corrected CORS configuration:
-  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000'; //Get from environment variables if available.  Use default otherwise
   app.enableCors({
-    origin: '*',
+    origin: 'https://simple-form-jk56-5bpqg80oh-nomas-projects-7fd2d87b.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Specify allowed HTTP methods
     allowedHeaders: ['*'],
     credentials: true, // Needed if you're sending cookies (like with sessions)
