3bbb77294fe9dc9322cbae4782a5a2767e5703b5
Nam Phan <pphnam21@clc.fitus.edu.vn>
2024-11-05 21:42:43 +0700

    chore: change header req

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   // Corrected CORS configuration:
   const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000'; //Get from environment variables if available.  Use default otherwise
   app.enableCors({
-    origin: frontendUrl, // Only allow requests from your frontend application's URL
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Specify allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    allowedHeaders: ['*'],
     credentials: true, // Needed if you're sending cookies (like with sessions)
   });
 
